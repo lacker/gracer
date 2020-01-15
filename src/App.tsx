634f2a91284a00af8e2352677cecd977d7208122
Kevin Lacker <lacker@gmail.com>
2020-01-14 16:20:30 -0800

    3 hooked up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
 import "./App.css";
 
-function App() {
-  return <p>hello world</p>;
+function Thing() {
+  let ref: any = useRef();
+  useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01));
+  return (
+    <mesh
+      ref={ref}
+      onClick={e => console.log("click")}
+      onPointerOver={e => console.log("hover")}
+      onPointerOut={e => console.log("unhover")}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshNormalMaterial attach="material" />
+    </mesh>
+  );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Canvas>
+      <Thing />
+    </Canvas>
+  );
+}
