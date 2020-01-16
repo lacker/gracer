82c8be779f3c431b7a4864b16216af599458b799
Kevin Lacker <lacker@gmail.com>
2020-01-16 12:45:31 -0800

    stuff

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,15 @@
+type Vertex = { x: number; y: number; edges: number[] };
+
+function randomCoordinate() {
+  return -10 + Math.random() * 20;
+}
+
 export default class Graph {
   constructor() {
     this.vertices = [];
+  }
+
+  addRandomVertex() {
+    //
   }
 }
