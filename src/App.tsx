40c470cb551ec0ad733e13bc97b7eff623055e9a
Kevin Lacker <lacker@gmail.com>
2020-01-16 11:38:44 -0800

    larger

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,20 @@ function Thing() {
   );
 }
 
+function Ball() {
+  return (
+    <mesh visible position={[1, 2, 3]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+}
+
 export default function App() {
   return (
-    <Canvas>
+    <Canvas style={{ height: "100vh" }}>
       <Thing />
+      <Ball />
     </Canvas>
   );
 }
