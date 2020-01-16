c3050b25174d9ab7a32676980892e8d12c851eaf
Kevin Lacker <lacker@gmail.com>
2020-01-16 15:20:41 -0800

    graph stuff

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -9,7 +9,51 @@ export default class Graph {
     this.vertices = [];
   }
 
+  hasEdge(i: number, j: number): boolean {
+    let v = this.vertices[i];
+    if (!v) {
+      return false;
+    }
+    for (let out of v.edges) {
+      if (out === j) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  chooseVertex(): number {
+    return Math.floor(this.vertices.length * Math.random());
+  }
+
   addRandomVertex() {
-    //
+    this.vertices.push({
+      x: randomCoordinate(),
+      y: randomCoordinate(),
+      edges: []
+    });
+  }
+
+  isEmpty(): boolean {
+    return this.vertices.length === 0;
+  }
+
+  addRandomEdge() {
+    if (this.vertices.length < 2) {
+      return;
+    }
+
+    for (let count = 0; count < 1000; count++) {
+      let i = this.chooseVertex();
+      let j = this.chooseVertex();
+      if (i === j || this.hasEdge(i, j)) {
+        continue;
+      }
+      this.vertices[i].edges.push(j);
+      this.vertices[j].edges.push(i);
+      return;
+    }
+
+    throw new Error("graph is overedged");
   }
 }
