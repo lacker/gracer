c0742c54f6e7143efb7b1f9fc1bed2abfdbd551c
Kevin Lacker <lacker@gmail.com>
2020-01-16 12:06:13 -0800

    Vertex

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,34 @@ import React, { useRef } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
 import "./App.css";
 
-function Ball(props: { x: number; y: number }) {
+function Vertex(props: { x: number; y: number }) {
   return (
-    <mesh visible position={[props.x, props.y, -5]} rotation={[0, 0, 0]}>
-      <sphereGeometry attach="geometry" args={[1, 32, 32]} />
+    <mesh visible position={[props.x, props.y, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[0.2, 32, 32]} />
       <meshNormalMaterial attach="material" />
     </mesh>
   );
 }
 
 export default function App() {
+  let coords = [];
+  for (let i = -10; i <= 10; i++) {
+    coords.push(i);
+  }
   return (
-    <Canvas style={{ height: "100vh", backgroundColor: "#333333" }}>
-      <Ball x={-10} y={-10} />
-      <Ball x={-10} y={-5} />
-      <Ball x={-10} y={0} />
-      <Ball x={-10} y={5} />
-      <Ball x={-10} y={10} />
-      <Ball x={-5} y={0} />
-      <Ball x={0} y={0} />
-      <Ball x={5} y={0} />
-      <Ball x={10} y={0} />
+    <Canvas
+      resize={{ scroll: false }}
+      orthographic
+      style={{ height: "100vh", backgroundColor: "#222222" }}
+      camera={{ zoom: 50 }}
+    >
+      {coords.map(y => (
+        <Vertex x={-10} y={y} />
+      ))}
+      <Vertex x={-5} y={0} />
+      <Vertex x={0} y={0} />
+      <Vertex x={5} y={0} />
+      <Vertex x={10} y={0} />
     </Canvas>
   );
 }
