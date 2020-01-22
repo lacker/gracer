116cb3de95166126b4203a7dad85f78364a03baa
Kevin Lacker <lacker@gmail.com>
2020-01-22 11:32:47 -0800

    separate out GraphView

---
+++ src/GraphView.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { useFrame } from "react-three-fiber";
+
+import Graph from "./Graph";
+
+function Ball(props: { x: number; y: number }) {
+  return (
+    <mesh visible position={[props.x, props.y, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[0.2, 32, 32]} />
+      <meshLambertMaterial color="#0000ff" attach="material" />
+    </mesh>
+  );
+}
+
+function Rod(props: { x1: number; y1: number; x2: number; y2: number }) {
+  let x = (props.x1 + props.x2) / 2;
+  let y = (props.y1 + props.y2) / 2;
+  let dx = props.x2 - props.x1;
+  let dy = props.y2 - props.y1;
+  let dist = Math.sqrt(dx * dx + dy * dy);
+  let angle = Math.atan2(dy, dx);
+  return (
+    <mesh visible position={[x, y, 0]} rotation={[0, 0, angle]}>
+      <boxGeometry attach="geometry" args={[dist, 0.1, 0.1]} />
+      <meshLambertMaterial color="#777777" attach="material" />
+    </mesh>
+  );
+}
+
+export default function GraphView(props: { graph: Graph }) {
+  let [ticks, setTicks] = useState(0);
+  useFrame(() => {
+    props.graph.step();
+    setTicks(ticks + 1);
+  });
+  return (
+    <>
+      {props.graph.vertices().map(v => (
+        <Ball x={v.x} y={v.y} key={Math.random()} />
+      ))}
+
+      {props.graph.edges().map(([v1, v2]) => (
+        <Rod x1={v1.x} y1={v1.y} x2={v2.x} y2={v2.y} key={Math.random()} />
+      ))}
+    </>
+  );
+}
