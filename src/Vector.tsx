5ee78eed827ee159e4308d3b1dd55cbd1c98276a
Kevin Lacker <lacker@gmail.com>
2020-02-06 10:29:56 -0800

    vectors are 3d

--- src/Vector.tsx
+++ src/Vector.tsx
@@ -3,10 +3,10 @@ export default class Vector {
   y: number;
   z: number;
 
-  constructor(x: number, y: number) {
+  constructor(x: number, y: number, z: number) {
     this.x = x;
     this.y = y;
-    this.z = 0;
+    this.z = z;
   }
 
   toString(): string {
@@ -14,23 +14,23 @@ export default class Vector {
   }
 
   static zero(): Vector {
-    return new Vector(0, 0);
+    return new Vector(0, 0, 0);
   }
 
   static epsilon(): Vector {
-    return new Vector(0.01 * Math.random(), 0.01 * Math.random());
+    return new Vector(0.01 * Math.random(), 0.01 * Math.random(), 0);
   }
 
   static random(): Vector {
-    return new Vector(-10 + 20 * Math.random(), -10 + 20 * Math.random());
+    return new Vector(-10 + 20 * Math.random(), -10 + 20 * Math.random(), 0);
   }
 
   add(other: Vector): Vector {
-    return new Vector(this.x + other.x, this.y + other.y);
+    return new Vector(this.x + other.x, this.y + other.y, this.z + other.z);
   }
 
   scale(k: number): Vector {
-    return new Vector(k * this.x, k * this.y);
+    return new Vector(k * this.x, k * this.y, k * this.z);
   }
 
   dot(other: Vector): number {
@@ -53,11 +53,12 @@ export default class Vector {
     return this.scale(k / len);
   }
 
-  // Rotates anticlockwise.
+  // Rotates anticlockwise around the z-axis.
   rotate(radians: number): Vector {
     return new Vector(
       Math.cos(radians) * this.x - Math.sin(radians) * this.y,
-      Math.sin(radians) * this.x + Math.cos(radians) * this.y
+      Math.sin(radians) * this.x + Math.cos(radians) * this.y,
+      this.z
     );
   }
 }
