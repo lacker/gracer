3d3be29075829f86036040e0f5a16e917816896e
Kevin Lacker <lacker@gmail.com>
2020-02-13 09:22:11 -0800

    Camera component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from "react";
-import { Canvas } from "react-three-fiber";
+import React, { useEffect, useRef } from "react";
+import { Canvas, useThree } from "react-three-fiber";
 import "./App.css";
 
 import EmbeddedGraph from "./EmbeddedGraph";
 import GraphView from "./GraphView";
 import PlanarGraph from "./PlanarGraph";
+
+function Camera(props: any) {
+  const ref = useRef<any>();
+  const { setDefaultCamera } = useThree();
+  useEffect(
+    () => {
+      setDefaultCamera(ref.current);
+    },
+    [setDefaultCamera]
+  );
+  return <perspectiveCamera ref={ref} {...props} />;
+}
 
 export default function App() {
   let graph = new PlanarGraph();
@@ -25,8 +37,8 @@ export default function App() {
       }}
       resize={{ scroll: false }}
       style={{ height: "100vh", backgroundColor: "#eeeeee" }}
-      camera={{ position: [0, 0, 20] }}
     >
+      <Camera position={[0, 0, 20]} />
       <pointLight position={[-20, 50, 100]} />
       <ambientLight intensity={0.5} />
       <GraphView graph={embedded} />
